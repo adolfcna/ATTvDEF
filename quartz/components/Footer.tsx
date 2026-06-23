731fd473223145b2711c0d7ffa7474c67034950c
adolfcna <adolfcna@gmail.com>
2026-06-24 01:55:36 +0330

    fix footer component

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,27 +1,25 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
-
-export default ((opts?: Options) => {
-  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
+export default (() => {
+  const Footer: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
+
     return (
-        <footer class={`${displayClass ?? ""}`}>
+      <footer class={`${displayClass ?? ""}`}>
         <p>
           © {year} ATTvDEF | Offensive Security • Defensive Intelligence | CNA
         </p>
         <ul>
           <li>
-          <a href="https://github.com/adolfcna">GitHub</a>
+            <a href="https://github.com/adolfcna">GitHub</a>
           </li>
           <li>
-          <a href="https://www.linkedin.com/in/adolfcna/">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/adolfcna/">LinkedIn</a>
           </li>
         </ul>
-        </footer>
-        )
+      </footer>
+    )
   }
 
   Footer.css = style
